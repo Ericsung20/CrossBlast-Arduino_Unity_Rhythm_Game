3274bbe40ccbde99da9f0594af83259ace2e1229
Eric <ericsung17@gmail.com>
2026-03-19 20:06:38 -0500

    CrossBlast file upload

---
+++ SolvedIssue/NoteMakingIssue/NoteMakingIssue/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public Image Panel;
+    float time = 0f;
+    float F_time = 1f;
+
+    public void Fade()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        Panel.gameObject.SetActive(true);
+        time = 0f;
+        Color alpha = Panel.color;
+        while (alpha.a < 1f) //ÆäÀÌµå¾Æ¿ô
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        time = 0f;
+        SceneManager.LoadScene("Game");
+        yield return null;
+    }
+
+}
